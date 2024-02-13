8520416f10245dd7bd23b2a02ab3da7e496585ee
Thulle Lima <158754520+ThulleLima@users.noreply.github.com>
2024-02-12 22:59:18 -0300

    Update main2.js

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -30,3 +30,21 @@ function leiaMais2(){
 		btnLeiaMais2.innerHTML="Leia Menos";
 	}
 }
+
+
+jQuery(document).ready(function(){
+
+	jQuery(window).scroll(function(){
+	  if (jQuery(this).scrollTop() > 100) {
+		jQuery('a[href="#top"]').fadeIn();
+	  } else {
+		jQuery('a[href="#top"]').fadeOut();
+	  }
+	});
+  
+	jQuery('a[href="#top"]').click(function(){
+	  jQuery('html, body').animate({scrollTop : 0},800);
+	  return false;
+	});
+  
+  });	
